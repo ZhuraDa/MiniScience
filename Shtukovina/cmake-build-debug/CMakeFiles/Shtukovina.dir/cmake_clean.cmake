981ec940010e2d7fffd4f58761b6f00c41bc57b4
Daniil <zhuravlev.ds@phystech.edu>
2021-02-28 22:53:16 +0300

    First commit

---
+++ Shtukovina/cmake-build-debug/CMakeFiles/Shtukovina.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Shtukovina.dir/main.cpp.obj"
+  "Shtukovina.exe"
+  "Shtukovina.exe.manifest"
+  "Shtukovina.pdb"
+  "libShtukovina.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Shtukovina.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
