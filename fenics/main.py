34166d24610f2eb2d58afd4255712631753a2d4d
ZhuraDa <zhuravlev.ds@phystech.edu>
2021-03-10 22:19:15 +0300

    new_fenics

--- fenics/main.py
+++ fenics/main.py
@@ -6,19 +6,18 @@ V = FunctionSpace(mesh, "Lagrange", 1)
 
 # Define Dirichlet boundary (x = 0 or x = 1)
 def boundary(x):
-    return x[0] < DOLFIN_EPS*pow(10,8) or x[0] > 2 - DOLFIN_EPS*pow(10,8)
+    return x[0] < DOLFIN_EPS or x[0] > 1 - DOLFIN_EPS
 print(DOLFIN_EPS)
 # Define boundary condition
-u0 = Constant(0.0)
+u0 = Expression("1+x[0]*x[0]+x[1]*x[1]",degree=2)
 bc = DirichletBC(V, u0, boundary)
 
 # Define variational problem
 u = TrialFunction(V)
 v = TestFunction(V)
 #f = Expression("10*exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)", degree=2)
-f = Expression("x[1]*sin(x[0])", degree=2)
-g = Expression("exp(-100*(x[0]*x[0]+x[1]*x[1]))", degree=2)
-g = Expression("exp(-100*(x[0]*x[0]+x[1]*x[1]))", degree=2)
+f = Expression("50*x[1]*cos(x[0])", degree=2)
+g = Expression("100*exp(x[0]*x[0]+x[1]*x[1])", degree=2)
 a = inner(grad(u), grad(v))*dx
 L = f*v*dx + g*v*ds
 
